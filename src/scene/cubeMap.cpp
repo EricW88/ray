260bae07937949da1928aa88c656ec2fef7bce82
ericwang <ericwang2838@utexas.edu>
2023-02-09 16:01:45 -0600

    cubemapping done

--- src/scene/cubeMap.cpp
+++ src/scene/cubeMap.cpp
@@ -10,41 +10,40 @@ glm::dvec3 CubeMap::getColor(ray r) const
 {
 	// YOUR CODE HERE
 	// FIXME: Implement Cube Map here
-	glm::dvec3 d = glm::normalize(r.getDirection());
-	double x = abs(d.x);
-	double y = abs(d.y);
-	double z = abs(d.z);
+	glm::dvec3 d = r.getDirection();
+	// std::cout << glm::to_string(d) << std::endl;
+	double abs_x = abs(d.x);
+	double abs_y = abs(d.y);
+	double abs_z = abs(d.z);
 	// std::cout << d.x << " " << d.y << " " << d.z << std::endl;
-	if(x >= y && x >= z) {
-		int u;
-        int v;
+	if(abs_x > abs_y && abs_x > abs_z) {
+		
 		if(d.x > 0) {
-			std::cout << "pos x" << std::endl;
-			glm::dvec2 coord(d.y, -d.z);
+			glm::dvec2 coord((-d.z / abs_x + 1) / 2, (d.y / abs_x + 1) / 2);
 			return tMap[0]->getMappedValue(coord);
 		} else {
 			// std::cout << "neg x" << std::endl;
-            u = ((d.z / d.x) + 1) / 2;
-            v = ((d.y / d.x) + 1) / 2;
-			glm::dvec2 coord(u, v);
+
+			glm::dvec2 coord((d.z / abs_x + 1) / 2, (d.y / abs_x + 1) / 2);
+			// std::cout << coord.x << " " << coord.y << std::endl;
 			return tMap[1]->getMappedValue(coord);
 		}
-	} else if (y >= x && y >= z) {
-		std::cout << "y" << std::endl;
+	} else if (abs_y > abs_x && abs_y > abs_z) {
+		// std::cout << "y" << std::endl;
 		if(d.y > 0) {
-			glm::dvec2 coord(-d.z, d.x);
+			glm::dvec2 coord((d.x / abs_y + 1) / 2, (-d.z / abs_y + 1) / 2);
 			return tMap[2]->getMappedValue(coord);
 		} else {
-			glm::dvec2 coord(d.z, d.x);
+			glm::dvec2 coord((d.x / abs_y + 1) / 2, (d.z / abs_y + 1) / 2);
 			return tMap[3]->getMappedValue(coord);
 		}
 	} else {
-		std::cout << "z" << std::endl;
+		// std::cout << "z" << std::endl;
 		if(d.z > 0) {
-			glm::dvec2 coord(d.y, d.x);
+			glm::dvec2 coord((d.x / abs_z + 1) / 2, (d.y / abs_z + 1) / 2);
 			return tMap[4]->getMappedValue(coord);
 		} else {
-			glm::dvec2 coord(d.y, -d.x);
+			glm::dvec2 coord((-d.x / abs_z + 1) / 2, (d.y / abs_z + 1) / 2);
 			return tMap[5]->getMappedValue(coord);
 		}
 	}
